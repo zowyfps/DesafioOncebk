ac2977334515d86ceaccdc31d9b82efaa2894707
zowyfps <1rociozoe@gmail.com>
2023-01-18 23:01:54 -0300

    json

--- server.js
+++ server.js
@@ -1,121 +1,65 @@
-//----------------Requires----------------
-const { Contenedor } = require("./managers/Contenedor");
-const { generateProducts } = require("./managers/randomProducts");
+import express from "express";
+import handlebars from "express-handlebars";
+import {Server} from "socket.io";
+import path from "path";
+import {fileURLToPath} from 'url';
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+import { dbOptions } from "./config/dbConfig.js";
 
-const express = require("express");
-const handlebars = require("express-handlebars");
+import { productRouter } from "./routes/api/products.js";
+import { clientRouter } from "./routes/web/clientRoutes.js";
+import { authRouter } from "./routes/web/authRouter.js";
+import { productsSocket } from "./routes/ws/products.js";
+import { chatSocket } from "./routes/ws/chat.js";
 
-//----------------Productos (faker.js) y Contenedores----------------
-const productosRandoms = generateProducts();
-const leerComentarios = new Contenedor("./files/chat.json");
-const guardarComentarios = new Contenedor(
-	"./files/mensajesSinNormalizar.json"
-);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-//----------------io y http----------------
-const { Server: HttpServer } = require("http");
-const { Server: IoServer } = require("socket.io");
-
+//Express server
 const app = express();
-const httpServer = new HttpServer(app);
-const io = new IoServer(httpServer);
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const port = process.env.PORT || 8080;
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
 
-// ----------------Mensajes----------------
+//Configuracion template engine handlebars
+app.engine(".hbs",handlebars.engine({extname: '.hbs'}));
+app.set('views', __dirname+'/views');
+app.set("view engine", ".hbs");
 
-io.on("connection", async socket => {
-	let mensajesChat = await leerComentarios.getAll();
-	console.log("Se contectó un usuario");
+app.use(cookieParser());
+//confiugracion de session
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: dbOptions.mongoAtlasSessions.urlDatabase
+    }),
+    secret:"claveSecreta",
+    //indicamos un almacenamiento externo
+    resave: false,
+    saveUninitialized: false
+}));
 
-	const text = {
-		text: "ok",
-		mensajesChat
-	};
 
-	socket.emit("mensaje-servidor", text);
+// Server routes
+// Api routes
+app.use('/api/productos',productRouter);
+//view routes
+app.use(clientRouter);
+app.use(authRouter);
 
-	socket.on("mensaje-nuevo", async (msg, cb) => {
-		mensajesChat.push(msg);
-		const text = {
-			text: "mensaje nuevo",
-			mensajesChat
-		};
+//Express server
+const server = app.listen(8080,()=>{
+    console.log('listening on port 8080')
+})
 
-		io.sockets.emit("mensaje-servidor", text);
-		await guardarComentarios.save({
-			author: msg.author,
-			text: msg.text
-		});
-	});
-});
 
-// ----------------Mensajes cambios por json----------------
-app.get("/api/mensajes/:id", async (req, res) => {
-	const { id } = req.params;
-	const productoById = await productos.getById(id);
-	productoById
-		? res.json(productoById)
-		: res.json({ error: "Producto no encontrado" });
-});
-
-app.put("/api/mensajes/:id", async (req, res) => {
-	const { id } = req.params;
-	const respuesta = await guardarComentarios.updateById(id, req.body);
-	res.json(respuesta);
-});
-
-app.delete("/api/mensajes/:id", async (req, res) => {
-	const { id } = req.params;
-	res.json(await guardarComentarios.deleteById(id));
-});
-
-app.delete("/api/texts", async (req, res) => {
-	res.json(await guardarComentarios.deleteAll());
-
-});
-
-// ----------------Render handlebars ----------------
-
-app.set("view engine", "hbs");
-app.set("views", "./views/layouts");
-
-app.use(express.static("public"));
-
-app.engine(
-	"hbs",
-	handlebars.engine({
-		extname: ".hbs",
-		defaultLayout: "",
-		layoutsDir: "",
-		partialsDir: __dirname + "/views/partials"
-	})
-);
-
-// ----------------Productos----------------
-app.get("/api/productos-test", async (req, res) => {
-	const producto = await productosRandoms;
-	res.render("productos", {
-		list: producto,
-		listExist: true,
-		producto: true
-	});
-});
-
-app.get("/", async (req, res) => {
-	const producto = await productosRandoms;
-	res.render("index", {
-		titulo: "Productos de Crud",
-		list: producto,
-		listExist: true,
-		producto: true
-	});
-});
-
-//----------------Por si el server tiene errores o se hostea correctamente ----------------
-httpServer.listen(port, err => {
-	if (err) throw new Error(`Error al iniciar el servidor: ${err}`);
-	console.log(`Server is running on port ${port}`);
+//Websocket server
+const io = new Server(server);
+//configuracion websocket
+io.on("connection",async(socket)=>{
+    // console.log('Nuevo cliente conectado!');
+    //PRODUCTOS
+    productsSocket(socket, io.sockets);
+    //CHAT
+    chatSocket(socket, io.sockets);
 });
